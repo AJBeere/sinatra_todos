4d268cd170db4ad713e90142ab4da27359a1b24c
Tony Beere <tonybeere87@gmail.com>
2021-05-19 18:58:02 +0700

    update 1

--- database_persistence.rb
+++ database_persistence.rb
@@ -2,12 +2,18 @@ require "pg"
 
 class DatabasePersistence
   def initialize(logger)
-    @db = PG.connect(dbname: "todos")
-    @logger = logger
+    @db = if Sinatra::Base.production?
+            PG.connect(ENV['DATABASE_URL'])
+          else
+            PG.connect(dbname: "todos")
+          end
+  end
+
+  def disconnect
+    @db.close
   end
 
   def query(statement, *params)
-    @logger.info "#{statement}: #{params}"
     @db.exec_params(statement, params)
   end
 
